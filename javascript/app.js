a243009d03297b3b85cb522aa528efaab4025c84
Arlindo Gomes Azevedo Junior <arlindo.g3@gmail.com>
2022-09-07 11:07:09 -0300

    site novo

--- javascript/app.js
+++ javascript/app.js
@@ -1,92 +1,82 @@
-const loadProducts = (produtos, IdDivParent ) => {
-    const parentDiv = document.querySelector(IdDivParent)
-    produtos.forEach(produto => {
-
-        const html = `
+const loadProducts = (produtos, idDivParent) => {
+    const parentDiv = document.querySelector(idDivParent)
+    produtos.forEach( produto => {
+      const html = `
         <article class="prato">
-                    <img src="${produto.image}" alt="${produto.title}">
-                    <h4>${produto.title}</h4>
-                    <h4>${produto.value}</h4>
-                    <p>${produto.description}</p>
-                    <button type="button" onclick='modalTrigger(${produto.id})'>Quero este Prato</button>
-                </article>
-        `
-         
-        parentDiv.insertAdjacentHTML('beforeend', html)
+          <img src="${produto.image}" alt="${produto.title}">
+          <h4>${produto.title}</h4>
+          <h4>R$ ${produto.value}</h4>
+          <p>${produto.description}</p>
+          <button type="button" onclick="modalTrigger(${produto.id})">Quero este prato</button>
+        </article>
+      `
+      parentDiv.insertAdjacentHTML('beforeend', html)
     })
-}
-
-const modalTrigger = (productId) => {
+  }
+  
+  const modalTrigger = (productId) => {
     const modal = document.querySelector('.modal')
-
+  
     if (productId != null) {
-        const produto = produtos.filter( produto => produto.id == productId)[0]
-
-        if (produto != null) {
-            modal.querySelector('#title').value = produto.title
-        }
+      const produto = produtos.filter( produto => produto.id == productId )[0]
+      if (produto != null) {
+        modal.querySelector('#title').value = produto.title
+      }
     }
-
-    modal.classList.contains('hide') == true ? modal.classList.remove('hide') :  modal.classList.add('hide')
-}
-
-const whatsappLinkGenerator = (phoneNumber, productTitle, productQuantity, buyerName, buyerAddress, buyerPayment) => `https://api.whatsapp.com/send?phone=${phoneNumber}&text=Olá eu quero: ${productQuantity} ${productTitle} - Entregar para ${buyerName} no endereço: 
-${buyerAddress} - A forma de pagamento será: ${buyerPayment}`
-
-const checKout = phoneNumber => {
+  
+    modal.classList.contains('hide') == true ? modal.classList.remove('hide') : modal.classList.add('hide')
+  }
+  
+  const whatsappLinkGenerator = (phoneNumber, productTitle, productQuantity, buyerName, buyerAddress, buyerPayment) => `https://api.whatsapp.com/send?phone=${phoneNumber}&text=Olá eu quero: ${productQuantity} ${productTitle} - Entregar para ${buyerName} no endereco: ${buyerAddress} - A forma de pagamento será: ${buyerPayment}`
+  
+  const checkout = phoneNumber => {
     const form = document.querySelector('#form-product')
-
+  
     form.addEventListener('submit', e => {
-        e.preventDefault()
-
-        const productTitle = form.querySelector('input#title').value
-        const productQuantity = form.querySelector('input#quantity').value
-        const buyerName = form.querySelector('input#name').value
-        const productAddress = form.querySelector('input#address').value
-        const productPayment = form.querySelector('select#payment').value
-        const whatsappUrl = whatsappLinkGenerator(phoneNumber, productTitle, productQuantity, buyerName, buyerAddress, buyerPayment)
-        
-        window.location.href = whatsappUrl
-    })    
-}
-
-const search = (products, searchTerm) => products.filter( product => product.title.includes(`${searchTerm}`) || 
-product.description.includes(`${searchTerm}`) )
-
-const LoadSearch = (form, productsDivId) => {
+      e.preventDefault()
+  
+      const productTitle = form.querySelector('input#title').value
+      const productQuantity = form.querySelector('input#quantity').value
+      const buyerName = form.querySelector('input#name').value
+      const buyerAddress = form.querySelector('input#address').value
+      const buyerPayment = form.querySelector('select#payment').value
+  
+      const whatsappUrl = whatsappLinkGenerator(phoneNumber, productTitle, productQuantity, buyerName, buyerAddress, buyerPayment)
+      window.location.href = whatsappUrl
+    })
+  }
+  
+  const search = (products, searchTerm) => products.filter( product => product.title.includes(`${searchTerm}`) || product.description.includes(`${searchTerm}`) )
+  
+  const loadSearch = (form, productsDivId) => {
     const productsDiv = document.querySelector(productsDivId)
     const inputSearch = form.querySelector('#inputSearch')
-
-   
-    form.addEventListener9('submit',(e) => {
-        e.preventDefault()
-        if (inputSearch.value != '') {
-          productsDiv.querySelectorAll('.prato').forEach(prato => {
-            prato.remove()
-          })
-
-          const results = search(produtos, inputSearch.value)
-
-          results.forEach( produto => {
-
-            const html = `
+  
+    form.addEventListener('submit', (e) => {
+      e.preventDefault()
+      if (inputSearch.value != '') {
+        productsDiv.querySelectorAll('.prato').forEach(prato => {
+          prato.remove()
+        })
+        const results = search(produtos, inputSearch.value)
+        
+        results.forEach( produto => {
+          const html = `
             <article class="prato">
-                        <img src="${produto.image}" alt="${produto.title}">
-                        <h4>${produto.title}</h4>
-                        <h4>${produto.value}</h4>
-                        <p>${produto.description}</p>
-                        <button type="button" onclick="modalTrigger(${produto.id})">Quero este Prato</button>
-                    </article>
-            `
-             
-            parentDiv.insertAdjacentHTML('beforeend', html)
-
-          })
-        }
-    })   
-}
-
-loadProducts(produtos,'#product-div')
-checKout('5511983501061')
-LoadSearch(document.querySelector('#formSearch'), '#product-div')
+              <img src="${produto.image}" alt="${produto.title}">
+              <h4>${produto.title}</h4>
+              <h4>R$ ${produto.value}</h4>
+              <p>${produto.description}</p>
+              <button type="button" onclick="modalTrigger(${produto.id})">Quero este prato</button>
+            </article>
+          `
+          productsDiv.insertAdjacentHTML('beforeend', html)
+        })
+      }
+    })
+  }
+  
+  loadProducts(produtos, '#product-div')
+  checkout('551198350-1061')
+  loadSearch(document.querySelector('#formSearch'), '#product-div')
 
--- javascript/produtos.js
+++ javascript/produtos.js
@@ -1,44 +1,87 @@
 const produtos = [
     {
-        id: 1,
-        image: 'https://baconmockup.com/640/360',
-        title: 'Produto 01',
-        description: 'Descrição do produto 01',
-        value: 'R$10.00',
+      id: 1,
+      image: 'https://baconmockup.com/640/360',
+      title: 'Produto 01',
+      description: 'Descricão do produto 01',
+      value: 100.00
     },
     {
-        id: 2,
-        image: 'https://baconmockup.com/640/360',
-        title: 'Produto 02',
-        description: 'Descrição do produto 02',
-        value: 'R$15.00',
+      id: 2,
+      image: 'https://baconmockup.com/640/360',
+      title: 'Produto 02',
+      description: 'Descricão do produto 02',
+      value: 20.00
     },
     {
-        id: 3,
-        image: 'https://baconmockup.com/640/360',
-        title: 'Produto 03',
-        description: 'Descrição do produto 03',
-        value: 'R$20.00',
+      id: 3,
+      image: 'https://baconmockup.com/640/360',
+      title: 'Produto 03',
+      description: 'Descricão do produto 03',
+      value: 30.00
     },
     {
-        id: 4,
-        image: 'https://baconmockup.com/640/360',
-        title: 'Produto 04',
-        description: 'Descrição do produto 04',
-        value: 'R$40.00',
+      id: 4,
+      image: 'https://baconmockup.com/640/360',
+      title: 'Produto 04',
+      description: 'Descricão do produto 04',
+      value: 40.00
     },
     {
-        id: 5,
-        image: 'https://baconmockup.com/640/360',
-        title: 'Produto 05',
-        description: 'Descrição do produto 05',
-        value: 'R$45.00',
+      id: 5,
+      image: 'https://baconmockup.com/640/360',
+      title: 'Produto 05',
+      description: 'Descricão do produto 05',
+      value: 50.00
     },
     {
-        id: 6,
-        image: 'https://baconmockup.com/640/360',
-        title: 'Produto 06',
-        description: 'Descrição do produto 06',
-        value: 'R$60.00',
+      id: 6,
+      image: 'https://baconmockup.com/640/360',
+      title: 'Produto 06',
+      description: 'Descricão do produto 06',
+      value: 60.00
     },
-]
+    {
+      id: 7,
+      image: 'https://baconmockup.com/640/360',
+      title: 'Produto 07',
+      description: 'Descricão do produto 07',
+      value: 70.00
+    },
+    {
+      id: 8,
+      image: 'https://baconmockup.com/640/360',
+      title: 'Produto 08',
+      description: 'Descricão do produto 08',
+      value: 80.00
+    },
+    {
+      id: 9,
+      image: 'https://baconmockup.com/640/360',
+      title: 'Produto 09',
+      description: 'Descricão do produto 09',
+      value: 90.00
+    },
+    {
+      id: 10,
+      image: 'https://baconmockup.com/640/360',
+      title: 'Produto 10',
+      description: 'Descricão do produto 10',
+      value: 200.00
+    },
+    {
+      id: 11,
+      image: 'https://baconmockup.com/640/360',
+      title: 'Produto 11',
+      description: 'Descricão do produto 11',
+      value: 310.00
+    },
+    {
+      id: 12,
+      image: 'https://baconmockup.com/640/360',
+      title: 'Produto 12',
+      description: 'Descricão do produto 12',
+      value: 410.00
+    }
+  ]
+    
